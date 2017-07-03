d7536b337c48dffec397aa4d759a02da8655cfe4
Christian Nagel <christian@christiannagel.com>
2017-07-03 12:30:54 +0200

    overflow

---
+++ OverflowSample/OverflowSample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OverflowSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.MaxValue;
+            checked
+            {
+                x++;
+                Console.WriteLine($"{x}, hex: {x:X}");
+            }
+        }
+    }
+}
