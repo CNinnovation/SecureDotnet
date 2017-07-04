c7a56a0a5c78417d758a2e047694d1ee65705266
Christian Nagel <christian@christiannagel.com>
2017-07-04 10:41:17 +0200

    configuration sample

---
+++ DotnetCoreConfigurationSample/DotnetCoreConfigurationSample/Startup.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace DotnetCoreConfigurationSample
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            if (env.IsDevelopment())
+            {
+                builder.AddUserSecrets<Startup>();
+            }
+
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();
+
+            app.Map("/config", app1 =>
+            {
+                app1.Run(async context =>
+                {
+                    string config1 = Configuration.GetSection("MySetting")["setting1"];
+                    string conn1 = Configuration.GetConnectionString("DefaultConnection");
+                    string secret1 = Configuration["mysecret1"];
+                    await context.Response.WriteAsync($"<h2>{config1}</h2><h3>{conn1}</h3><h4>{secret1}</h4>");
+                });
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("<h1>Hello World!</h1>");
+            });
+        }
+    }
+}
