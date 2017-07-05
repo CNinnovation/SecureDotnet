2688f11511facd31e3f84e3b4ac22ea69671083c
Christian Nagel <christian@christiannagel.com>
2017-07-05 10:39:05 +0200

    XSRF Demo

---
+++ XSRFDemo/XSRFDemo/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using XSRFDemo.Models;
+
+namespace XSRFDemo.Controllers
+{
+    [AutoValidateAntiforgeryToken]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult EditBook()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult EditBook(Book book)
+        {
+            return View("EditBookResult", book);
+        }
+    }
+}
